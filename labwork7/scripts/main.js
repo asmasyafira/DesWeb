d719393b980ad756482eb55262e882ad8e033acb
Asma Syafira <47140064+asmasyafira@users.noreply.github.com>
2022-06-20 22:07:25 +0700

    First Commit

---
+++ labwork7/scripts/main.js
@@ -0,0 +1,23 @@
+var myHeading = document.querySelector('h1');
+myHeading.innerHTML = 'Hello world!';
+
+// var myImage = document.querySelector('img');
+// var myButton = document.querySelector('button');
+// var myHeading = document.querySelector('h1');
+
+// myImage.onclick = function() {
+//     var mySrc = myImage.getAttribute('src');
+//     if(mySrc === 'images/firefox2.png') {
+//         myImage.setAttribute('src', 'images/facebook-icon.png');
+//     } else  {
+//         myImage.setAttribute('src', 'images/firefox-icon.png');
+//     }
+// }
+
+// function setUserName(){
+//     var myName = prompt('Please enter your name.');
+//     localStorage.setItem('name', myName);
+//     myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+//     //alert --> nampilin aja
+//     //prompt --> nambahin inputan
+// }
